f7e2a38b536ec5fa10fa6f9dfdcc594583ce4f0f
Ubuntu <andrii@jenkins.t3aauoqmxprejkxh4e40t2abhf.fx.internal.cloudapp.net>
2023-08-31 09:14:58 +0000

    first commit

---
+++ script.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+
+
+mkdir site1 site2 site3
+
+git clone -b main https://gitlab.com/staskuznetsov/site.git /home/andrii/site1
+git clone -b dev https://gitlab.com/staskuznetsov/site.git /home/andrii/site2
+git clone -b feature https://gitlab.com/staskuznetsov/site.git /home/andrii/site3
+
+ansible-playbook ansib.yaml
+
+
+
+sudo rm -R site1 site2 site3
