39ae5078b390dd5f1a557e1d4113c3022ddc8628
andrikovalenko <akovalenko19761214@outlook.com>
2023-09-01 08:20:52 +0000

    second version

--- script.sh
+++ script.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-
-
-
 mkdir site1 site2 site3
 
 git clone -b main https://gitlab.com/staskuznetsov/site.git /home/andrii/site1
@@ -10,7 +7,5 @@ git clone -b dev https://gitlab.com/staskuznetsov/site.git /home/andrii/site2
 git clone -b feature https://gitlab.com/staskuznetsov/site.git /home/andrii/site3
 
 ansible-playbook ansib.yaml
-
-
 
 sudo rm -R site1 site2 site3
